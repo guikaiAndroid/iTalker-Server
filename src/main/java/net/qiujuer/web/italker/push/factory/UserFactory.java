c1850ac0a2cebc1eb4509852c2088f0952012ade
guikai <916126728@qq.com>
2020-02-29 16:13:02 +0800

    获取好友列表、添加好友、获取好友信息、搜索好友接口完成

--- src/main/java/net/qiujuer/web/italker/push/factory/UserFactory.java
+++ src/main/java/net/qiujuer/web/italker/push/factory/UserFactory.java
@@ -2,11 +2,14 @@ package net.qiujuer.web.italker.push.factory;
 
 import com.google.common.base.Strings;
 import net.qiujuer.web.italker.push.bean.db.User;
+import net.qiujuer.web.italker.push.bean.db.UserFollow;
 import net.qiujuer.web.italker.push.utils.Hib;
 import net.qiujuer.web.italker.push.utils.TextUtil;
 
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 /**
  * Description: 用户操作逻辑实现
@@ -201,6 +204,109 @@ public class UserFactory {
         password = TextUtil.getMD5(password);
         // 再进行一次对称的Base64加密，当然可以采取加盐的方案
         return TextUtil.encodeBase64(password);
+    }
+
+    /**
+     * 获取我的联系人的列表
+     *
+     * @param self User
+     * @return List<User>
+     */
+    public static List<User> contacts(User self) {
+        return Hib.query(session -> {
+            // 重新加载一次用户信息到self中，和当前的session绑定
+            session.load(self, self.getId());
+
+            // 获取我关注的人
+            Set<UserFollow> flows = self.getFollowing();
+
+            // 使用简写方式
+            return flows.stream()
+                    .map(UserFollow::getTarget)
+                    .collect(Collectors.toList());
+
+        });
+    }
+
+    /**
+     * 关注人的操作
+     *
+     * @param origin 发起者
+     * @param target 被关注的人
+     * @param alias  备注名
+     * @return 被关注的人的信息
+     */
+    public static User follow(final User origin, final User target, final String alias) {
+        UserFollow follow = getUserFollow(origin, target);
+
+        if (follow != null) {
+            // 已关注，直接返回
+            return follow.getTarget();
+        }
+
+        return Hib.query(session -> {
+            // 想要操作懒加载的数据，需要重新load一次
+            session.load(origin, origin.getId());
+            session.load(target, target.getId());
+
+            // 我关注人的时候，同时他也关注我，
+            // 所有需要添加两条UserFollow数据
+            UserFollow originFollow = new UserFollow();
+            originFollow.setOrigin(origin);
+            originFollow.setTarget(target);
+            // 备注是我对他的备注，他对我默认没有备注
+            originFollow.setAlias(alias);
+
+            // 发起者是他，我是被关注的人的记录
+            UserFollow targetFollow = new UserFollow();
+            targetFollow.setOrigin(target);
+            targetFollow.setTarget(origin);
+
+            // 保存数据库
+            session.save(originFollow);
+            session.save(targetFollow);
+
+            return target;
+        });
+    }
+
+    /**
+     * 查询两个人是否已经关注
+     *
+     * @param origin 发起者
+     * @param target 被关注人
+     * @return 返回中间类UserFollow
+     */
+    public static UserFollow getUserFollow(final User origin, final User target) {
+        return Hib.query(session -> (UserFollow) session
+                .createQuery("from UserFollow where originId = :originId and targetId = :targetId")
+                .setParameter("originId", origin.getId())
+                .setParameter("targetId", target.getId())
+                .setMaxResults(1)
+                // 唯一查询返回
+                .uniqueResult());
+    }
+
+    /**
+     * 搜索联系人的实现
+     *
+     * @param name 查询的name，允许为空
+     * @return 查询到的用户集合，如果name为空，则返回最近的用户
+     */
+    @SuppressWarnings("unchecked")
+    public static List<User> search(String name) {
+        if (Strings.isNullOrEmpty(name))
+            name = ""; // 保证不能为null的情况，减少后面的一下判断和额外的错误
+        final String searchName = "%" + name + "%"; // 模糊匹配
+        return Hib.query(session -> {
+            // 查询的条件：name忽略大小写，并且使用like（模糊）查询；
+            // 头像和描述必须完善才能查询到
+            return (List<User>) session.createQuery(
+                    "from User where lower(name) like :name and portrait is not null and description is not null")
+                    .setParameter("name", searchName)
+                    .setMaxResults(20) // 至多20条
+                    .list();
+        });
     }
 
 }
